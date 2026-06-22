1b56c4b678d246a37fb0ee8799b6794c3d8fcae4
Prasad <prasadbonthu87@gmail.com>
2026-06-22 06:14:53 +0200

    Add new settings to IPipelineWidgetSettings interface

--- src/components/PipelinesWidgetConfig/IPipelineWidgetSettings.ts
+++ src/components/PipelinesWidgetConfig/IPipelineWidgetSettings.ts
@@ -4,8 +4,13 @@ export interface IPipelineWidgetSettings {
   showSucceeded: boolean;
   showFailed: boolean;
   showAverage: boolean;
+  showAverageWaitTime: boolean;
   showCanceled: boolean;
   showAsPercentage: boolean;
+  showSuccessTrend: boolean;
+  showLastRunStatus: boolean;
+  showTriggeredBy: boolean;
+  showTopBranch: boolean;
   selectedProjects: string[];
   renderMultipleProjects: boolean;
 }
